56ed4d546654ab42526af88f059225ed450aae46
Marcelo Távora <marcelovtneves@gmail.com>
2023-08-06 12:52:16 -0300

    Primeiro commit

---
+++ src/main/java/ascf/ascf/AscfApplication.java
@@ -0,0 +1,13 @@
+package ascf.ascf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ascf/ascf/AscfApplicationTests.java
@@ -0,0 +1,13 @@
+package ascf.ascf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
